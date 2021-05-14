437ed0f008c5cdf2f9f46f6dccdb6c8e011e4300
evrenonur <onur.evren.1999@gmail.com>
2021-05-14 22:48:34 +0300

    Add Product Detail Function

--- Trendyol.php
+++ Trendyol.php
@@ -8,8 +8,11 @@ class Trendyol
 
     private $curl;
     private $responseData;
-    private $base_url = "https://public.trendyol.com/discovery-web-searchgw-service/v2/api/";
+    private $base_url = "https://public.trendyol.com/";
+    private $api_v2 = "discovery-web-searchgw-service/v2/api/";
+    private $api = "discovery-web-productgw-service/api/";
     private $infinite_scroll = "infinite-scroll/";
+
     private $pageCount = 208;
     private $whileCheck = true;
     private $whileCounter = 0;
@@ -22,7 +25,7 @@ class Trendyol
     }
 
     public function infinite_scroll($path){
-        $this->url = $this->base_url.$this->infinite_scroll.$path;
+        $this->url = $this->base_url.$this->api_v2.$this->infinite_scroll.$path;
         $this->curl->get($this->url);
         if ($this->curl->error){
             return [
@@ -47,9 +50,9 @@ class Trendyol
     public function infinite_scrolls($path){
         while ($this->whileCheck){
             if ($this->whileCounter == 0){
-                $this->url = $this->base_url.$this->infinite_scroll.$path;
+                $this->url = $this->base_url.$this->api_v2.$this->infinite_scroll.$path;
             }else{
-                $this->url = $this->base_url.$this->infinite_scroll.$path."&pi=".$this->whileCounter;
+                $this->url = $this->base_url.$this->api_v2.$this->infinite_scroll.$path."&pi=".$this->whileCounter;
             }
             $this->curl->get($this->url);
             if ($this->curl->error){
@@ -84,6 +87,28 @@ class Trendyol
         }
         return  $this->responseData;
 
+    }
+
+    public function product_detail($id,$merchantId,$campaignId){
+        $this->url = $this->base_url.$this->api."productDetail/".$id."?merchantId=".$merchantId."&campaignId=".$campaignId;
+        $this->curl->get($this->url);
+        if ($this->curl->error){
+            return [
+                'code' => $this->curl->errorCode,
+                'message' => $this->curl->errorMessage,
+                'url' => $this->url
+            ];
+        }else{
+            if ($this->curl->response->statusCode == 200){
+                return $this->curl->response->result;
+            }else{
+                return [
+                    'code' => $this->curl->response->statusCode,
+                    'message' => $this->curl->response->error,
+                    'url' =>    $this->url
+                ];
+            }
+        }
     }
 
 
--- test.php
+++ test.php
@@ -4,5 +4,5 @@ include "Trendyol.php";
 $trendyol = new Trendyol();
 
 echo "<pre>";
-print_r($trendyol->infinite_scroll("sr?wg=1&wb=109791&lc=1004&fl=encoksatanurunler"));
+print_r($trendyol->product_detail("43374789","108658","61"));
 echo "</pre>";
